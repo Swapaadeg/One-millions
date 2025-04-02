9637898d23c3ca7ea51b0508ea4f79e23ea4dbb8
Marie Rivier <marie.rivier23@gmail.com>
2025-04-02 09:37:28 +0200

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["src/**/*.{html,js}","*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        arvo: ['arvo'],
+        helvetica: ['helvetica']
+      }
+    },
   },
   plugins: [],
 }
